4899205218f8543c49e9a619ba20e5c3bf2f0bda
Peerayukun <Peerayuphengsuwan@gmail.com>
2024-05-01 13:13:35 +0700

    project model

--- demo/daijai/project/models.py
+++ demo/daijai/project/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from estimate_item.models import EstimateItem
+
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ProjectEstimateItem(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    estimate_item = models.ForeignKey(
+        EstimateItem, on_delete=models.PROTECT, null=True, blank=True
+    )
+    estimate_item_amount = models.IntegerField(default=1)
