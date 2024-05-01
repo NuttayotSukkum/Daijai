145649188c646062d9286cee4a9280b371d93906
Peerayukun <Peerayuphengsuwan@gmail.com>
2024-05-01 12:58:37 +0700

    estimate item model

--- demo/daijai/estimate_item/admin.py
+++ demo/daijai/estimate_item/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from estimate_item.models import EstimateItemType, EstimateItemMaterial, EstimateItem
+
 
 # Register your models here.
+@admin.register(EstimateItemType)
+class EstimateItemTypeAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in EstimateItemType._meta.fields]
+
+
+@admin.register(EstimateItemMaterial)
+class EstimateItemMaterialTypeAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in EstimateItemMaterial._meta.fields]
+
+
+@admin.register(EstimateItem)
+class EstimateItemAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in EstimateItem._meta.fields]
--- demo/daijai/estimate_item/models.py
+++ demo/daijai/estimate_item/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from material.models import Material
+
 
 # Create your models here.
+class EstimateItemType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class EstimateItem(models.Model):
+    estimate_item_type = models.ForeignKey(
+        EstimateItemType, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return " - ".join([self.name, self.code])
+
+
+class EstimateItemMaterial(models.Model):
+    estimate_item = models.ForeignKey(EstimateItem, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.PROTECT)
+    material_amount = models.IntegerField(default=1)
